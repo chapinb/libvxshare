644ea7087ce4d34336c140533446eb945f84bc9e
Chapin Bryce <mail@chapinb.com>
2020-08-11 08:03:13 -0400

    Updated doc references to module without install

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../libvxshare'))
+sys.path.insert(0, os.path.abspath('../../libvxshare'))
 
 
 # -- Project information -----------------------------------------------------
